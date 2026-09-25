56e6f6ae331ff7840ecd79d9bc13c1cf8b041231
medoed <alxev2010@mail.ru>
2026-09-25 18:15:46 +0500

    Create Program.cs

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // I think this is 9/8 kyu :D but why can't I write this? I fkng love FIBONACCI!
+            Console.WriteLine(Fibonacci(10));
+            
+        }
+
+        static int Fibonacci(int number)
+        {
+
+            int counter = 1;
+            var a = 0;
+            var b = 1;
+            
+            while (number > counter)
+            {
+                var tempA = a; 
+                
+                a = b;
+                b = tempA + b;
+                
+                counter++;
+            }
+            
+            return a;
+        }
+        
+    }
+}
